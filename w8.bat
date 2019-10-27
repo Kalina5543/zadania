a97535f7cf8ef3bfd961bc477e2a388a2c09f950
Kalina5543 <56843529+Kalina5543@users.noreply.github.com>
2019-10-27 17:38:21 +0100

    Update w8.bat
    p

--- w8.bat
+++ w8.bat
@@ -15,8 +15,8 @@ echo czy zmienic nazwe folderu?
 echo jezeli tak wybierz 3
 echo jezeli nie wynierz 2
 set/p "wybor=>>"
-if %wybor%==3 goto3
-if %wybor%==2 goto2
+if %wybor%==3 goto 3
+if %wybor%==2 goto 2
 :2
 echo dziekuje za uwage
 pause
@@ -33,8 +33,8 @@ echo czy przeniesc folder?
 echo jezeli tak wybierz 4
 echo jezeli nie wybierz 2
 set/p "wybor=>>"
-if %wybor%==4 goto4
-if %wybor%==2 goto2
+if %wybor%==4 goto 4
+if %wybor%==2 goto 2
 :4
 cls
 echo wpisz lokalizacja dolecowa
